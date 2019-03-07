ad7cacfd7ae5c3a3ed33e045ffe6a20aab4fc9d8
alialyami <alidalyami@gmail.com>
2019-03-07 08:53:20 +0300

    Homework W02 D04

--- exercise/js/main.js
+++ exercise/js/main.js
@@ -15,7 +15,8 @@
  */
 
 function question1() {
-	$("replaceMe").css("replaceMe", "replaceMe");
+	$("p").css("color", "blue");
+	
 }
 
 $("#button1").on('click', question1);
@@ -26,11 +27,11 @@ $("#button1").on('click', question1);
  * Uncomment the code below and solve the problem
  */
 
-	// function question2(){
-	// 	$("replaceMe").replaceMe("replaceMe");
-	// }
+	function question2(){
+		$("h2").text("Ali");
+	}
 
-	// $("#button1").on('click', question2);
+	$("#button1").on('click', question2);
 
 /**
  * Question 3:
@@ -39,11 +40,11 @@ $("#button1").on('click', question1);
  * Uncomment the code below and solve the problem
  */
 
-	// function question3(){
-	// 	$("replaceMe").html("replaceMe");
-	// }
+	function question3(){
+		$(".falseFact").html("True Fact");
+	}
 
-	// $("#button1").on('click', question3);
+	$("#button1").on('click', question3);
 
 
 //SECTION 2: Now you are on your own. Write the necessary code to accomplish the following.
@@ -52,20 +53,29 @@ $("#button1").on('click', question1);
  * Question 4:
  * When button 2 is clicked, change the background color of the whole page to "pink"
  */
-
-
+function question4(){
+	$("*").css("background-color","pink")
+}
+	$("#button2").on('click',question4)
 
 /**
  * Question 5:
  * When button 2 is clicked, change the color of all h2's to "green"
  */
-
-
-
+	function question5(){
+		 $("h2").css("color","green")
+		}
+		$("#button2").on('click',question5)
 /**
  * Question 6:
  * When button 2 is clicked, change the html content of all "blockquotes" to be "<span>no quote</span>"
  */
+		function question6(){
+			$("blockquote").html("<span>no quote</span>")
+		}
+ $("#button2").on('click',question6)
+
+
 
 
 //SECTION 3: This section is more difficult. Try your hand at the next 3 questions.
@@ -74,7 +84,10 @@ $("#button1").on('click', question1);
  * Question 7:
  * When button 3 is clicked, change the text in the header's h1 tag to read "jQuery Ninja"
  */
-
+function question7(){
+	$("h1").text("jQuery Ninja")
+}
+$("button3").on('click',question7)
 
 /**
  * Question 8:
